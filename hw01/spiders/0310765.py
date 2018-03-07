d106aa4f59e7f0e83fe69a265e856e4d933b8e09
tsaichengchin <p097524320@gmail.com>
2018-03-07 16:15:09 +0800

    ignore [公告]

--- hw01/spiders/0310765.py
+++ hw01/spiders/0310765.py
@@ -16,10 +16,10 @@ class PttSpider(scrapy.Spider):
 			url = div.xpath('div[@class="r-ent"]/div[@class="title"]/a/@href').extract()
 		with open('all_articles.txt', 'a') as f:
 			for i in range(len(title)):
-  				f.write('{0},{1},{2}\n'.format(date[i],title[i],url[i]))
+				if title[i].find("[公告]")== -1:
+					f.write('{0},{1},{2}\n'.format(date[i],title[i],url[i]))
 		if len(response.xpath('//*[@id="action-bar-container"]/div/div[2]/a[2]').extract()) > 0:
 			path = response.xpath('//*[@id="action-bar-container"]/div/div[2]/a[2]/@href').extract()
 			target_url = 'https://www.ptt.cc'
 			url = target_url + path[0];
-			
 			yield scrapy.Request(url, callback=self.parse)
